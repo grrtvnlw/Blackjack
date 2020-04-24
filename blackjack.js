31fec67548a22731290c865938af9e06e3ad4e09
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-24 16:33:45 -0400

    break time

--- blackjack.js
+++ blackjack.js
@@ -6,6 +6,10 @@ const points = [1, 2, 3, 4, 5, 5, 7, 8, 8, 10, 11, 12, 13]
 let dealerHandArray = [];
 let playerHandArray = [];
 
+// Assigning score values for Player and Dealer
+let dScore = 0;
+let pScore = 0;
+
 // function to create our deck
 function makeDeck() {
   let deck = [];
@@ -111,9 +115,35 @@ function scoreKeeper(array) {
 console.log(playerHandArray)
 console.log(dealerHandArray)
 
-// Build the hit function
-function hitMe(deck) {
+// Build the bust function
+function bust() {
+  return "You lose"
+  
+}
 
+// Build the hit function
+function hitPlayer(array) {
+    newCard = deck.pop()
+    array.push(newCard)
+    if (scoreKeeper(array) > 21) {
+      console.log("Player busted");
+      hit.removeEventListener('click', function(e){});
+      return
+    } else {
+    return array
+    }
+}
+
+function hitDealer(array) {
+  newCard = deck.pop()
+  array.push(newCard)
+  if (scoreKeeper(array) > 21) {
+    console.log("Dealer busted")
+    hit.removeEventListener('click', function(e){});
+    return
+  } else {
+  return array
+  }
 }
 
 // deal event listener
@@ -121,42 +151,55 @@ const deal = document.querySelector('#deal-button');
 deal.addEventListener('click', function(e){
   const dealerHand = document.querySelector("#dealer-hand");
   const playerHand = document.querySelector("#player-hand");
-  const dealerScore = document.querySelector("#dealer-points");
-  const playerScore = document.querySelector("#player-points");
-  dealDeck(deck)
-  const cardImageOne = document.createElement('img');
-  const cardImageTwo = document.createElement('img');
-  const cardImageThree = document.createElement('img');
-  const cardImageFour = document.createElement('img');
-  cardImageOne.src = getCardImageURL(dealerHandArray[0])
-  cardImageTwo.src = getCardImageURL(dealerHandArray[1])
-  cardImageThree.src = getCardImageURL(playerHandArray[0])
-  cardImageFour.src = getCardImageURL(playerHandArray[1])
-  dealerHand.appendChild(cardImageOne)
-  dealerHand.appendChild(cardImageTwo)
-  playerHand.appendChild(cardImageThree)
-  playerHand.appendChild(cardImageFour)
-  dealerScore.innerHTML = renderScore(scoreKeeper(dealerHandArray));
-  playerScore.innerHTML = renderScore(scoreKeeper(playerHandArray))
+  const dealerPoints = document.querySelector("#dealer-points");
+  const playerPoints = document.querySelector("#player-points");
+  if (dealerPoints.innerHTML == ""){
+    dealDeck(deck)
+    const cardImageOne = document.createElement('img');
+    const cardImageTwo = document.createElement('img');
+    const cardImageThree = document.createElement('img');
+    const cardImageFour = document.createElement('img');
+    cardImageOne.src = getCardImageURL(dealerHandArray[0])
+    cardImageTwo.src = getCardImageURL(dealerHandArray[1])
+    cardImageThree.src = getCardImageURL(playerHandArray[0])
+    cardImageFour.src = getCardImageURL(playerHandArray[1])
+    dealerHand.appendChild(cardImageOne)
+    dealerHand.appendChild(cardImageTwo)
+    playerHand.appendChild(cardImageThree)
+    playerHand.appendChild(cardImageFour)
+    dealerScore = renderScore(scoreKeeper(dealerHandArray));
+    playerScore = renderScore(scoreKeeper(playerHandArray));
+    dealerPoints.innerHTML = dealerScore;
+    playerPoints.innerHTML = playerScore;
+  }
 })
 
 const hit = document.querySelector('#hit-button');
 hit.addEventListener('click', function(e){
   const playerHand = document.querySelector("#player-hand");
   const dealerHand = document.querySelector("#dealer-hand");
+  const dealerPoints = document.querySelector("#dealer-points");
+  const playerPoints = document.querySelector("#player-points");
   // const cardImage = renderImage(getCardImageURL(deck[2]))
-
-  const cardImage = document.createElement('img');
-  cardImage.src = getCardImageURL(deck[4])
-  // cardImage.setAttribute('src', "/images/jack_of_clubs2.png")
-  // playerHand.innerHTML = cardImage;
-  playerHand.appendChild(cardImage)
-  dealerHand.appendChild(cardImage)
+  hitDealer(dealerHandArray)
+  const cardImageDealer = document.createElement('img');
+  cardImageDealer.src = getCardImageURL(dealerHandArray[dealerHandArray.length - 1])
+  dealerHand.appendChild(cardImageDealer)
+  dealerScore = renderScore(scoreKeeper(dealerHandArray));
+  dealerPoints.innerHTML = dealerScore;
+  // if (dealerScore > 21) {
+  //   console.log("Dealer Bust")
+  // }
+  hitPlayer(playerHandArray)
+  const cardImagePlayer = document.createElement('img');
+  cardImagePlayer.src = getCardImageURL(playerHandArray[playerHandArray.length - 1])
+  playerHand.appendChild(cardImagePlayer)
+  playerScore = renderScore(scoreKeeper(playerHandArray));
+  playerPoints.innerHTML = playerScore;
+  // if (playerScore > 21) {
+  //   console.log("Player Bust")
+  // }
 })
 
 // console.log(getDeck())
 
-
-
-
-// Get Image
