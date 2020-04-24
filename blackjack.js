ec96732dc5eba2620f598781ea3e689589e55d2c
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-24 06:57:26 -0400

    Added deck shuffle function

--- blackjack.js
+++ blackjack.js
@@ -1,20 +1,63 @@
-// const button = document.querySelector('.buttons');
-// button.addEventListener('click', function(e){
-//   console.log(e);
-// })
+// Building the deck
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const points = [1, 2, 3, 4, 5, 5, 7, 8, 8, 10, 11, 12, 13]
+
+// function to create our deck
+function makeDeck() {
+  let deck = [];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < points.length; j++) {
+      const card = {point: points[j], suit: suits[i]};
+      deck.push(card);
+    }
+  }
+
+  return deck;
+}
+
+// shuffle function built using the Fisher-Yates Shuffle algorithm
+function shuffle(array) {
+  let currentIndex = array.length, tempValue, randomIndex;
+
+  // while the array is not empty
+  while (0 != currentIndex) {
+    // choose a random index
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // swap random element with the current element
+    tempValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = tempValue;
+  }
+
+  return array;
+
+}
+
+let deck = makeDeck()
+console.log(deck);
+shuffle(deck);
+console.log(deck);
+let dealerHandArray = [];
+let playerHandArray = [];
+
 const deal = document.querySelector('#deal-button');
 deal.addEventListener('click', function(e){
   console.log(e);
   const dealerHand = document.querySelector("#dealer-hand");
   const playerHand = document.querySelector("#player-hand");
+  const currentCard = deck[Math.floor(Math.random() * deck.length)]
+  const cardInHand = deck.pop(currentCard)
+  console.log(cardInHand)
   const cardImageOne = document.createElement('img');
   const cardImageTwo = document.createElement('img');
   const cardImageThree = document.createElement('img');
   const cardImageFour = document.createElement('img');
-  cardImageOne.setAttribute('src', "/images/QS.jpg")
-  cardImageTwo.setAttribute('src', "/images/5C.jpg")
-  cardImageThree.setAttribute('src', "/images/4H.jpg")
-  cardImageFour.setAttribute('src', "/images/QD.jpg")
+  cardImageOne.setAttribute('src', "/images/5_of_spades.png")
+  cardImageTwo.setAttribute('src', "/images/king_of_diamonds.png")
+  cardImageThree.setAttribute('src', "/images/8_of_clubs.png")
+  cardImageFour.setAttribute('src', "/images/9_of_diamonds.png")
   dealerHand.appendChild(cardImageOne)
   dealerHand.appendChild(cardImageTwo)
   playerHand.appendChild(cardImageThree)
@@ -26,25 +69,17 @@ hit.addEventListener('click', function(e){
   const playerHand = document.querySelector("#player-hand");
   // const dealerHand = document.querySelector("#dealer-hand");
   const cardImage = document.createElement('img');
-  cardImage.setAttribute('src', "/images/4H.jpg")
+  cardImage.setAttribute('src', "/images/jack_of_clubs2.png")
   playerHand.appendChild(cardImage)
   // dealerHand.appendChild(cardImage)
 })
 
-// Building the deck
-var suits = ["spades", "diamonds", "clubs", "hearts"];
-var points = [1, 2, 3, 4, 5, 5, 7, 8, 8, 10, 11, 12, 13]
+// console.log(getDeck())
 
-function getDeck() {
-  let deck = [];
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < points.length; j++) {
-      let card = {point: points[j], suit: suits[i]};
-      deck.push(card);
-    }
-  }
+// Deal the Deck
 
-  return deck;
+
+// Get Image
+function getCardImageURL(card) {
+
 }
-
-console.log(getDeck())
