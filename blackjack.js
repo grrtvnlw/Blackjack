04af2923ccae1e9422069759cb81d89ae683fa91
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-24 08:08:29 -0400

    Completed Step 7

--- blackjack.js
+++ blackjack.js
@@ -11,7 +11,6 @@ function makeDeck() {
       deck.push(card);
     }
   }
-
   return deck;
 }
 
@@ -30,26 +29,55 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = tempValue;
   }
-
   return array;
+}
 
+// build function for getting a card image URL
+function getCardImageURL(card) {
+  if (card.point < 2) {
+    // console.log `images/ace_of_${card.suit}.png`
+    return `/images/ace_of_${card.suit}.png`;
+  } else if (card.point <= 10) {
+    // console.log(card.suit, card.point);
+    return `/images/${card.point}_of_${card.suit}.png`;
+  } else if (card.point == 11) {
+    // console.log(card.suit, card.point);
+    return `/images/jack_of_${card.suit}.png`;
+  } else if (card.point == 12) {
+    // console.log(card.suit, card.point);
+    return `/images/queen_of_${card.suit}.png`;
+  } else if (card.point == 13) {
+    // console.log(card.suit, card.point);
+    return `/images/king_of_${card.suit}.png`;
+  }
+}
+
+// build a render function for rendering the card image
+function renderImage(url) {
+  return `
+    <div class="card">
+      <img src='${url}' />
+    </div>
+  `
 }
 
 let deck = makeDeck()
 console.log(deck);
 shuffle(deck);
-console.log(deck);
-let dealerHandArray = [];
-let playerHandArray = [];
+console.log(deck[2]);
+let url = getCardImageURL(deck[2])
+console.log(url)
+const test = document.querySelector('#messages');
+test.innerHTML = renderImage(url)
+// let dealerHandArray = [];
+// let playerHandArray = [];
 
 const deal = document.querySelector('#deal-button');
 deal.addEventListener('click', function(e){
   console.log(e);
   const dealerHand = document.querySelector("#dealer-hand");
   const playerHand = document.querySelector("#player-hand");
-  const currentCard = deck[Math.floor(Math.random() * deck.length)]
-  const cardInHand = deck.pop(currentCard)
-  console.log(cardInHand)
+  const currentCard = deck.pop()
   const cardImageOne = document.createElement('img');
   const cardImageTwo = document.createElement('img');
   const cardImageThree = document.createElement('img');
@@ -80,6 +108,3 @@ hit.addEventListener('click', function(e){
 
 
 // Get Image
-function getCardImageURL(card) {
-
-}
