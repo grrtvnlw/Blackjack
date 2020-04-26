91ecbde4ab731120a41b0b7034ed78e69c6c6177
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-26 17:02:53 -0400

    added Modal

--- blackjack.js
+++ blackjack.js
@@ -6,9 +6,23 @@ const points = [1, 2, 3, 4, 5, 5, 7, 8, 8, 10, 11, 12, 13]
 let dealerHandArray = [];
 let playerHandArray = [];
 
+function getInput(e) {
+  e.preventDefault();
+  document.getElementById("player-money").innerHTML = (document.getElementById("userInput").value)
+}  
+
+const form = document.querySelector('#theform');
+form.addEventListener('submit', getInput);
+
+form.addEventListener('submit', assignMoney);
+
 // Assigning result variables for bet payout
-let counterMoney = 500;
+let counterMoney = 0;
 let counterBet = 0;
+
+function assignMoney() {
+  counterMoney = parseInt(document.getElementById("player-money").innerHTML, 10);
+} 
 
 // Assigning global variables to DOM nodes in common use
 let dealerHand = document.querySelector("#dealer-hand");
@@ -357,3 +371,30 @@ bet.addEventListener('click', function(e){
     playerBet.innerHTML = counterBet;
   };
 });
+
+// Modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
